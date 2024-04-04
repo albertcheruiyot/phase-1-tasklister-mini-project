39612d92a65bc6cc4e10dc1b1c2b40d56231085f
Albert Cheruiyot <bettalbert99@gmail.com>
2024-04-04 21:08:10 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (Event) => {
   // your code here
+  doWork();
 });
+
+
+function doWork(){
+  const taskForm = document.getElementById('create-task-form');
+  const taskInput = document.getElementById('new-task-description');
+  const taskList = document.getElementById('tasks');
+
+  taskForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== '') {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskText;
+
+      taskList.appendChild(taskItem);
+
+      taskInput.value = '';
+    }
+  });
+}
